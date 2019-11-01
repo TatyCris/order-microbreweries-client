b2f8f5f85864e7451890a8a99f10dee98959cfa0
TATY <tatyccsilva@gmail.com>
2019-11-01 17:15:49 +0100

    Change heroku url

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,3 +1,3 @@
 export const mapbox_host = 'api.mapbox.com'
 export const localhost = 'http://localhost:4000'
-export const heroku = 'https://git.heroku.com/microbreweries.git'
+export const heroku = 'https://microbreweries.herokuapp.com'
